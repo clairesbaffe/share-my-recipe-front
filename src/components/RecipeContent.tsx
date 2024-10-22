aa7067dc911f3a27a3f3f6436b2a90dc6510a726
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-10-22 11:37:55 +0200

    FIX data display on recipe content page

--- src/components/RecipeContent.tsx
+++ src/components/RecipeContent.tsx
@@ -3,14 +3,16 @@ import { UserIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
 const RecipeContent = ({ recipe }: { recipe: any }) => {
-  const hours = Math.floor(recipe.preparation_time / 60);
-  const minutes = recipe.preparation_time % 60;
+  const recetteData = JSON.parse(recipe.recette);
+
+  const hours = Math.floor(recipe.preparationTime / 60);
+  const minutes = recipe.preparationTime % 60;
 
   let preparationTime;
-  if (recipe.preparation_time >= 60) {
+  if (recipe.preparationTime >= 60) {
     preparationTime = minutes === 0 ? `${hours} h` : `${hours} h ${minutes} mn`;
   } else {
-    preparationTime = `${recipe.preparation_time} mn`;
+    preparationTime = `${recipe.preparationTime} mn`;
   }
 
   // Ajust header card position according to tags number
@@ -24,50 +26,61 @@ const RecipeContent = ({ recipe }: { recipe: any }) => {
       <header className="relative w-full h-56 bg-[url('https://picsum.photos/800/200')] bg-cover font-artifika">
         <div
           className={`absolute left-1/2 transform -translate-x-1/2 bg-white rounded-xl p-8 w-3/5`}
-          style={{ bottom: `-${bottomValue}px`}}
+          style={{ bottom: `-${bottomValue}px` }}
         >
           <div className="flex flex-col">
             <div className="flex justify-around mb-5">
               <h1 className="text-2xl font-bold text-center">{recipe.title}</h1>
-              <span className="inline-flex items-center gap-1">
-                <Timer className="h-5 mr-1" />
-                {preparationTime}
-              </span>
-              <span className="inline-flex items-center gap-1">
-                <Utensils className="h-5 mr-1" />
-                {recipe.nb_persons} personnes
-              </span>
+              {recipe.preparationTime && (
+                <span className="inline-flex items-center gap-1">
+                  <Timer className="h-5 mr-1" />
+                  {preparationTime}
+                </span>
+              )}
+              {recipe.nbPersons && (
+                <span className="inline-flex items-center gap-1">
+                  <Utensils className="h-5 mr-1" />
+                  {recipe.nbPersons} personnes
+                </span>
+              )}
             </div>
             <div className="grid grid-cols-5 gap-5">
               <span className="col-span-1 inline-flex items-center gap-1">
                 <UserIcon className="h-5 mr-1" />
                 <Link to="/profile">{recipe.author.name}</Link>
               </span>
-              <span className="col-span-4 flex flex-wrap space-x-2 space-y-1">
-                {recipe.tags.map((tag: any, index: number) => (
-                  <span
-                    key={index}
-                    className="inline-flex items-center bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700"
-                  >
-                    {tag}
-                  </span>
-                ))}
-              </span>
+              {recipe.tags && (
+                <span className="col-span-4 flex flex-wrap space-x-2 space-y-1">
+                  {recipe.tags.map((tag: any, index: number) => (
+                    <span
+                      key={index}
+                      className="inline-flex items-center bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </span>
+              )}
             </div>
           </div>
-          <p className="text-sm italic w-3/5 bg-primary-light border-l-4 border-primary-dark shadow-lg p-6 rounded-lg mx-auto my-4">
-            "{recipe.description}"
-          </p>
+          {recipe.description && (
+            <p className="text-sm italic w-3/5 bg-primary-light border-l-4 border-primary-dark shadow-lg p-6 rounded-lg mx-auto my-4">
+              "{recipe.description}"
+            </p>
+          )}
         </div>
       </header>
 
-      <div className="m-16 p-4 grid grid-cols-4 gap-4" style={{ marginTop: `${marginTopValue}px`}}>
+      <div
+        className="m-16 p-4 grid grid-cols-4 gap-4"
+        style={{ marginTop: `${marginTopValue}px` }}
+      >
         <div className="col-span-1">
           <h2 className="text-2xl font-bold text-center font-artifika relative mb-6">
             Ingrédients
             <span className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-16 h-1 bg-yellow-500 rounded-full"></span>
           </h2>
-          {recipe.recipe.ingredients.map((ingredient: any, index: number) => (
+          {recetteData.ingredients.map((ingredient: any, index: number) => (
             <div className="flex flex-col" key={index}>
               <p className="font-secondary">{ingredient}</p>
             </div>
@@ -79,7 +92,11 @@ const RecipeContent = ({ recipe }: { recipe: any }) => {
             Instructions
             <span className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-16 h-1 bg-yellow-500 rounded-full"></span>
           </h2>
-          <p className="font-secondary">{recipe.recipe.instructions}</p>
+          {recetteData.instructions.map((instruction: any, index: number) => (
+            <div className="flex flex-col" key={index}>
+              <p className="font-secondary">{instruction}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/pages/Recipe.tsx
+++ src/pages/Recipe.tsx
@@ -3,85 +3,21 @@ import RecipeContent from "../components/RecipeContent";
 
 const Recipe = () => {
   const recipe = {
-    id: 1, // Ajout de l'ID
-    title: "Spaghetti Carbonara",
-    image: "https://example.com/spaghetti-carbonara.jpg",
+    title: "Soupe à l'oignon",
+    image:
+      "https://assets.afcdn.com/recipe/20181012/82641_w1024h1024c1cx2136cy1424cxt0cyt0cxb4272cyb2848.jpg",
     description:
-      "Un classique de la cuisine italienne, les spaghetti carbonara sont préparés avec des œufs, du fromage, de la pancetta et du poivre noir. Un plat réconfortant et savoureux qui ravira toute la famille.",
-    preparation_time: 30,
-    nb_persons: 4,
-    difficulty: 2,
-    tags: [
-      "Pâtes", 
-      "Végé", 
-      "Italien", 
-      // "Rapide", 
-      // "Facile", 
-      // //5
-      // "Confort Food", 
-      // "Repas de famille", 
-      // "Savourueux", 
-      // "Plat principal", 
-      // "Cuisiner avec des œufs", 
-      // //10
-      // "Cuisiner avec du fromage", 
-      // "Recette traditionnelle", 
-      // "Cuisine méditerranéenne", 
-      // "Régime omnivore", 
-      // "Cuisine réconfortante",
-      // //15
-      // "Cuisiner avec des restes",
-      // "Spaghetti", 
-      // "Repas en 30 minutes",
-      // "Cuisine à la maison",
-      // "Recette simple",
-      // //20
-      // "Délicieux",
-      // "Pâtes", 
-      // "Végé", 
-      // "Italien", 
-      // "Rapide", 
-      // //25
-      // "Facile", 
-      // "Confort Food", 
-      // "Repas de famille", 
-      // "Savourueux", 
-      // "Plat principal", 
-      // //30
-      // "Cuisiner avec des œufs", 
-      // "Cuisiner avec du fromage", 
-      // "Recette traditionnelle", 
-      // "Cuisine méditerranéenne", 
-      // "Régime omnivore", 
-      // //35
-      // "Cuisine réconfortante",
-      // "Cuisiner avec des restes",
-      // "Spaghetti", 
-      // "Repas en 30 minutes",
-      // "Cuisine à la maison",
-      // //40
-      // "Recette simple",
-      // "Délicieux",
-    ],
-    ratings: [5, 4, 1, 1], // Changement de "notes" à "ratings"
+      "La soupe à l'oignon est un classique de la cuisine française.",
+    recette:
+      '{"ingredients": ["Oignons", "Bouillon", "Pain grillé"], "instructions": ["Faire revenir les oignons.", "Ajouter le bouillon et mijoter."]}',
+    preparationTime: 1.2,
+    nbPersons: 2,
+    difficulty: 3.4,
+    tags: ["végé", "miam"],
+    ratings: 4.0,
     author: { name: "Chef Luigi", experience: "10 ans" },
-    date: new Date(),
-    recipe: { // Nouveau champ ajouté
-      ingredients: [
-        "Spaghetti",
-        "Pancetta",
-        "Œufs",
-        "Parmesan",
-        "Poivre noir",
-        "Sel"
-      ],
-      instructions: [
-        "Faites cuire les spaghetti selon les instructions du paquet. Dans une poêle, faites revenir la pancetta jusqu'à ce qu'elle soit croustillante. Dans un bol, battez les œufs avec le parmesan et le poivre. Ajoutez les spaghetti cuits à la poêle avec la pancetta, puis retirez du feu. Incorporez le mélange d'œufs et de fromage aux spaghetti chauds. Servez immédiatement, garni de fromage supplémentaire si désiré."
-      ]
-    }
-};
-
-  
+    date: "2024-10-22",
+  };
 
   return <RecipeContent recipe={recipe} />;
 };
