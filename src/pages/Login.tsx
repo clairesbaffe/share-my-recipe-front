c8729f7d48d9ac75e9553f87f1cf4fb21ffeaae8
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-10-22 12:40:37 +0200

    FEAT login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,11 +2,7 @@ import Form from "../components/LoginForm";
 
 const Login = () => {
   return (
-    <div className="mt-5">
-      <h2 className="font-artifika text-3xl font-bold text-center mb-8 text-gray-800 relative">
-        Connexion
-        <span className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-16 h-1 bg-yellow-500 rounded-full"></span>
-      </h2>
+    <div className="mt-5 flex justify-center">
       <Form />
     </div>
   );
