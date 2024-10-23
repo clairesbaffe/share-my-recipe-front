0b496f00c7a9fafe03fbb2508ff1bc27c75872b5
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-10-23 15:33:15 +0200

    FEAT fetch all recipes on /

--- src/services/RecipeService.tsx
+++ src/services/RecipeService.tsx
@@ -1,12 +1,39 @@
 export const createRecipe = async (recipeData: any) => {
-    const response = await fetch('http://localhost:8080/api/v1/recipes', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(recipeData),
-    });
+  const response = await fetch("http://localhost:8080/api/v1/recipes", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(recipeData),
+  });
 
-    return response;
+  return response;
+};
+
+export const getAllRecipes = async () => {
+  const response = await fetch("http://localhost:8080/api/v1/recipes", {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return response.json();
+};
+
+export const getRecipeById = async (recipeId: string) => {
+  const response = await fetch(
+    `http://localhost:8080/api/v1/recipes/${recipeId}`,
+    {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return response.json();
 };
